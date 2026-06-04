b43964f716c88d1154f429969922cf228149b20d
Umut AKSOY <152719549+umutakksy@users.noreply.github.com>
2026-06-05 01:47:11 +0300

    groq ai 56

--- frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c554fb599bb25a34c1b92041f97d9d129908acac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98783b2bdf5556a33e8750a413bf3dfe6c4c20b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
